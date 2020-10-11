af533ca048d040ecd5a6c986727984d11e61d1a2
Valeriu Seremet <valeriuseremet97@gmail.com>
2020-10-11 16:50:45 +0300

    Fix build

--- OnlineMenu.Api/Controllers/OrderController.cs
+++ OnlineMenu.Api/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using OnlineMenu.Application;
 
 namespace OnlineMenu.Api.Controllers
--- OnlineMenu.Api/Startup.cs
+++ OnlineMenu.Api/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OnlineMenu.Api.ExceptionHandling;
+using OnlineMenu.Application;
 
 namespace OnlineMenu.Api
 {
